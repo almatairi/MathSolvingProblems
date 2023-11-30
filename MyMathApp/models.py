d3fcf712aed14f1efef86355fc904a430c7df2b2
almatairi <almtahirii@gmail.com>
2023-11-30 23:32:42 +0100

    models added

--- MyMathApp/models.py
+++ MyMathApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+class Posts(models.Model):
+    name = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+class Difficulty(models.Model):
+    title = models.CharField(max_length=255)
