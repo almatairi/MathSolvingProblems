fd4c7337ee0516dc3cce1bf3556a90fb1d6a8c33
almatairi <almtahirii@gmail.com>
2023-12-01 21:17:51 +0100

    hm?

--- MyMathApp/admin.py
+++ MyMathApp/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, User
 
 
 # Register your models here.
 class PostsAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
+
+class UserAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(Post, PostsAdmin)
+admin.site.register(User, UserAdmin)
